24588bc9d269f04e0e532e3e89e022be9470c753
Enzey <enzey@enzey.net>
2014-05-14 20:05:49 -0500

    Create Directive
     * Renders values with normal mustache syntax.
     * Create no binds or watches

---
+++ noBind.js
@@ -0,0 +1,49 @@
+(function(angular, $) {
+
+	var module = angular.module('net.enzey.bindonce', []);
+
+	var ctrlBind = function($parse) {
+		return function($scope) {
+			$scope.$watch = function(varExpression, func) {
+				// varExpression.exp is passed as the 'old' value
+				//   so that angular can remove the expression text from an
+				//   element's attributes, such as when using an expression for the class.
+				func($parse(varExpression)($scope), varExpression.exp);
+			};
+		};
+	};
+
+	var linkBind = function($timeout) {
+		return function(scope, element, attrs) {
+			element.find('*').removeClass('ng-binding');
+			$timeout(function() {
+				scope.$destroy();
+			}, 0, false);
+		};
+	};
+
+	module.directive('nzNoBind', function ($parse, $timeout) {
+		return {
+			restrict: 'A',
+			priority: 999999,
+			scope: true,
+			controller: ctrlBind($parse),
+			link: function(scope, element, attrs) {
+				element.removeClass('ng-scope');
+				element.removeClass('ng-binding');
+				linkBind($timeout)(scope, element, attrs);
+			}
+		};
+	});
+
+	module.directive('nzNoBindChildren', function ($parse, $timeout) {
+		return {
+			restrict: 'A',
+			priority: -999999,
+			scope: true,
+			controller: ctrlBind($parse),
+			link: linkBind($timeout)
+		};
+	});
+
+})(angular, jQuery);
